ec9084e0f1c4717c93c6fd2bbd63fd52ac159a72
Bryce Caine <brycecaine@gmail.com>
2015-09-22 21:28:06 -0600

    Added csv-download functionality

--- wsgi/readitdown/readingtracker/views.py
+++ wsgi/readitdown/readingtracker/views.py
@@ -2,13 +2,16 @@ from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 from django.contrib.auth.models import User, Group
 from django.db.models import Q
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
+from django.views.generic.base import View
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from main.models import Friendship
 from readingtracker import service
 from readingtracker.forms import EntryForm
 from readingtracker.models import Entry
+import csv
 import json
 
 class HomeView(FormView):
@@ -69,9 +72,42 @@ class MonitorView(TemplateView):
         context['columns'] = json.dumps(table_data['columns'])
         context['data'] = json.dumps(table_data['data'])
         context['period'] = '%s %s' % (period, year)
-        context['next_month_month'] = next_month_month
-        context['next_month_year'] = next_month_year
         context['prev_month_month'] = prev_month_month
         context['prev_month_year'] = prev_month_year
+        context['month'] = month
+        context['year'] = year
+        context['next_month_month'] = next_month_month
+        context['next_month_year'] = next_month_year
 
         return context
+
+class FriendEntriesJSONView(View):
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        year = int(self.kwargs.get('year', datetime.now().year))
+        month = int(self.kwargs.get('month', datetime.now().month))
+
+        table_data = service.get_friend_entries(user, 'teacher', year, month)
+
+        data = {'data': table_data}
+
+        return JsonResponse(data)
+
+class FriendEntriesCSVView(View):
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        year = int(self.kwargs.get('year', datetime.now().year))
+        month = int(self.kwargs.get('month', datetime.now().month))
+
+        table_data = service.get_friend_entries(user, 'teacher', year, month)
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="list_%s_%s.csv"' % (year, month)
+
+        writer = csv.writer(response)
+        columns = [d['title'] for d in table_data['columns']]
+        writer.writerow(columns)
+        for row in table_data['data']:
+            writer.writerow(row)
+
+        return response
